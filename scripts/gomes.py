2b798840955466bc09f1d5f572542bc3c5a3985b
Davi <davi@pop-os.localdomain>
2020-02-09 08:24:34 -0300

    Definidas e concatenadas janelas de EEG pra treino

--- scripts/gomes.py
+++ scripts/gomes.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 col = ['path','freq','duração','montage']      
 
-train = pd.DataFrame(data=np.load('info_train.npy'),columns=col)
+train = pd.DataFrame(data=np.load('/home/davi/Documentos/tuh_seizures/scripts/info_train.npy'),columns=col)
 
 print(train.head())
